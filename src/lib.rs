4963861051e44654336abd37652ed85a45698dd0
Vignesh Sankaran <developer@sankaran.io>
2016-12-23 16:44:17 +1100

    Initial commit i.e. something that builds

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+#![feature(proc_macro)]
+
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate diesel_codegen;
+extern crate dotenv;
+
+pub mod schema;
+pub mod models;
+
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use dotenv::dotenv;
+use std::env;
+
+pub fn establish_connection() -> PgConnection
+{
+    dotenv().ok();
+
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&db_url).expect(&format!("Error connecting to {}", db_url))
+}
