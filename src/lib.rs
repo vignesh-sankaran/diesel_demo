f95713deace24d210a5c306c6a2febf05b25744c
Vignesh Sankaran <developer@sankaran.io>
2016-12-24 19:59:39 +1100

    Added writing post functionality

--- src/lib.rs
+++ src/lib.rs
@@ -12,10 +12,29 @@ use diesel::pg::PgConnection;
 use dotenv::dotenv;
 use std::env;
 
+use self::models::{Post, NewPost};
+
 pub fn establish_connection() -> PgConnection
 {
     dotenv().ok();
 
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&db_url).expect(&format!("Error connecting to {}", db_url))
+}
+
+pub fn create_post<'a>(conn: &PgConnection, title: &'a str, body: &'a str) -> Post
+{
+    use schema::posts;
+
+    let new_post = NewPost
+    {
+        title: title,
+        body: body,
+    };
+
+    diesel::insert(&new_post).into(posts::table)
+        .get_result(conn)
+        .expect("Error inserting new post into DB")
+
+
 }
--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,5 @@
+use super::schema::posts;
+
 #[derive(Queryable)]
 pub struct Post
 {
@@ -5,4 +7,12 @@ pub struct Post
     pub title: String,
     pub body: String,
     pub published: bool,
+}
+
+#[derive(Insertable)]
+#[table_name="posts"]
+pub struct NewPost<'a>
+{
+    pub title: &'a str,
+    pub body: &'a str,
 }
